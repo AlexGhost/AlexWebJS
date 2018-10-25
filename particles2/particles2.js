18a2b763e06708e139e844b156e957aa6edf54a8
acourtin <acourtin@student.42.fr>
2018-10-25 16:37:41 +0200

    meilleur degradé

--- particles2/particles2.js
+++ particles2/particles2.js
@@ -36,38 +36,50 @@ Particle.prototype.move = function() {
 
 Particle.prototype.draw = function() {
 	ctx.beginPath();
-	ctx.arc(this.x, this.y, 4, 0, Math.PI * 2, false);
+	ctx.arc(this.x, this.y, 3, 0, Math.PI * 2, false);
 	ctx.fillStyle = "white";
 	ctx.fill();
 	ctx.closePath();
 };
 
-function draw_line(x1, y1, x2, y2)
+Particle.prototype.draw_line = function(x2, y2)
 {
-	var distance = Math.sqrt(Math.abs(x1 - x2) * Math.abs(x1 - x2) + Math.abs(y1 - y2) * Math.abs(y1 - y2));
+	var distance = Math.sqrt(Math.abs(this.x - x2) * Math.abs(this.x - x2) + Math.abs(this.y - y2) * Math.abs(this.y - y2));
 	if (distance < 200)
 	{
 		ctx.beginPath();
-		if (distance < 50)
+		if (distance < 169)
 			ctx.strokeStyle = "#FFFFFF";
-		else if (distance < 60)
+		else if (distance < 171)
 			ctx.strokeStyle = "#EEEEEE";
-		else if (distance < 70)
+		else if (distance < 173)
+			ctx.strokeStyle = "#DDDDDD";
+		else if (distance < 175)
+			ctx.strokeStyle = "#CCCCCC";
+		else if (distance < 177)
+			ctx.strokeStyle = "#BBBBBB";
+		else if (distance < 179)
+			ctx.strokeStyle = "#AAAAAA";
+		else if (distance < 181)
+			ctx.strokeStyle = "#999999";
+		else if (distance < 183)
 			ctx.strokeStyle = "#888888";
-		else if (distance < 80)
+		else if (distance < 185)
+			ctx.strokeStyle = "#777777";
+		else if (distance < 187)
 			ctx.strokeStyle = "#666666";
-		else if (distance < 100)
+		else if (distance < 189)
 			ctx.strokeStyle = "#555555";
-		else if (distance < 120)
+		else if (distance < 191)
 			ctx.strokeStyle = "#444444";
-		else if (distance < 140)
+		else if (distance < 193)
 			ctx.strokeStyle = "#333333";
-		else if (distance < 160)
+		else if (distance < 195)
 			ctx.strokeStyle = "#222222";
 		else
 			ctx.strokeStyle = "#111111";
 		ctx.lineWidth = 2;
-		ctx.moveTo(x1, y1);
+		ctx.moveTo(this.x, this.y);
 		ctx.lineTo(x2, y2);
 		ctx.stroke();
 	}
@@ -111,7 +123,7 @@ function frame() {
 	for (var i = 0; i < particles.length; i++) {
 		particles[i].move();
 		for (var j = i + 1; j < particles.length; j++) {
-			draw_line(particles[i].x, particles[i].y, particles[j].x, particles[j].y);
+			particles[i].draw_line(particles[j].x, particles[j].y);
 		}
 	particles[i].draw();
 	}
